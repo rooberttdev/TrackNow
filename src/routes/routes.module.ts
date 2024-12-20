f3d62bbf94d38c70bdf192b601395624c1c75d48
Robert Lindoso <robert.lindoso@conecthus.org.br>
2024-12-20 13:04:43 -0400

    :sparkles: Feat -- Socket

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -4,10 +4,11 @@ import { RoutesController } from './routes.controller';
 import { MapsModule } from 'src/maps/maps.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { RoutesDriverService } from './routes-driver/routes-driver.service';
+import { RoutesDriverGateway } from './routes-driver/routes-driver.gateway';
 
 @Module({
   imports: [MapsModule, PrismaModule],
   controllers: [RoutesController],
-  providers: [RoutesService, RoutesDriverService],
+  providers: [RoutesService, RoutesDriverService, RoutesDriverGateway],
 })
 export class RoutesModule {}
